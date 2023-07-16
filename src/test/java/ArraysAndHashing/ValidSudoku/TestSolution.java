e9bcbd1346e0f9a4864ab32ef2af0a0e56bb60fd
AkshayChandole <akshaychandole1234@gmail.com>
2023-07-16 19:09:52 +0530

    valid-sudoku-solution

---
+++ src/test/java/ArraysAndHashing/ValidSudoku/TestSolution.java
@@ -0,0 +1,29 @@
+package ArraysAndHashing.ValidSudoku;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestSolution {
+    Solution solution = new Solution();
+
+    @Test
+    public void testCase1() {
+        char[][] board = {{'5', '3', '.', '.', '7', '.', '.', '.', '.'}, {'6', '.', '.', '1', '9', '5', '.', '.', '.'}, {'.', '9', '8', '.', '.', '.', '.', '6', '.'}, {'8', '.', '.', '.', '6', '.', '.', '.', '3'}, {'4', '.', '.', '8', '.', '3', '.', '.', '1'}, {'7', '.', '.', '.', '2', '.', '.', '.', '6'}, {'.', '6', '.', '.', '.', '.', '2', '8', '.'}, {'.', '.', '.', '4', '1', '9', '.', '.', '5'}, {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+        boolean expectedResult = true;
+        evaluateResult(board, expectedResult);
+
+    }
+
+    @Test
+    public void testCase2() {
+        char[][] board = {{'8', '3', '.', '.', '7', '.', '.', '.', '.'}, {'6', '.', '.', '1', '9', '5', '.', '.', '.'}, {'.', '9', '8', '.', '.', '.', '.', '6', '.'}, {'8', '.', '.', '.', '6', '.', '.', '.', '3'}, {'4', '.', '.', '8', '.', '3', '.', '.', '1'}, {'7', '.', '.', '.', '2', '.', '.', '.', '6'}, {'.', '6', '.', '.', '.', '.', '2', '8', '.'}, {'.', '.', '.', '4', '1', '9', '.', '.', '5'}, {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+        boolean expectedResult = false;
+        evaluateResult(board, expectedResult);
+    }
+
+    private void evaluateResult(char[][] board, boolean expectedResult) {
+        boolean actualResult = solution.isValidSudoku(board);
+        assertEquals(expectedResult, actualResult);
+    }
+}
