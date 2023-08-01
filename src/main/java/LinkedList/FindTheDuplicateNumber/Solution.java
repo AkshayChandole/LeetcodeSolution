ac18380410e781f8bac907cb14dda143fc604525
AkshayChandole <akshaychandole1234@gmail.com>
2023-08-01 16:25:20 +0530

    find-the-duplicate-number-solution

---
+++ src/main/java/LinkedList/FindTheDuplicateNumber/Solution.java
@@ -0,0 +1,93 @@
+package LinkedList.FindTheDuplicateNumber;
+
+import java.util.Arrays;
+
+// ***************************************************************
+// Optimal Solution using two pointers -
+
+/**
+ * Time Complexity - O(n)
+ * Space Complexity - O(n)
+ */
+public class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        fast = nums[0];
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
+
+// ***************************************************************
+// Better Solution using frequency array -
+
+/**
+ * Note - This solution uses extra space.
+ * <p>
+ * Time Complexity - O(n)
+ * Space Complexity - O(n)
+ */
+class Solution3 {
+    public int findDuplicate(int[] nums) {
+        int[] frequency = new int[nums.length + 1];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (frequency[nums[i]] == 0) frequency[nums[i]]++;
+            else return nums[i];
+        }
+
+        return -1;
+    }
+}
+
+// ***************************************************************
+// Better Solution using Array.Sort() -
+
+
+/**
+ * Note - This method involves modification in the array.
+ * <p>
+ * Time Complexity - O(n*log(n)+n)
+ * Space Complexity - O(1)
+ */
+class Solution2 {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) return nums[i];
+        }
+
+        return -1;
+    }
+}
+
+// ***************************************************************
+// Brute Force Solution -
+
+/**
+ * Time Complexity - O(n^2)
+ * Space Complexity - O(1)
+ */
+class Solution1 {
+    public int findDuplicate(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] - nums[j] == 0) return nums[i];
+            }
+        }
+        return -1;
+    }
+}
+
---
+++ src/test/java/LinkedList/FindTheDuplicateNumber/TestSolution.java
@@ -0,0 +1,35 @@
+package LinkedList.FindTheDuplicateNumber;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class TestSolution {
+    /*
+    Input: nums = [1,3,4,2,2]
+    Output: 2
+     */
+    @Test
+    public void TestCase1() {
+        int[] nums = new int[]{1, 3, 4, 2, 2};
+        int expectedResult = 2;
+        evaluateSolution(nums, expectedResult);
+    }
+
+    /*
+    Input: nums = [3,1,3,4,2]
+    Output: 3
+     */
+    @Test
+    public void TestCase2() {
+        int[] nums = new int[]{3, 1, 3, 4, 2};
+        int expectedResult = 3;
+        evaluateSolution(nums, expectedResult);
+    }
+
+    private void evaluateSolution(int[] nums, int expectedResult) {
+        Solution solution = new Solution();
+        int actualResult = solution.findDuplicate(nums);
+        assertEquals(expectedResult, actualResult);
+    }
+}
